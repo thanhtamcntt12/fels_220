8f65be9d4d6eb359f66a504ab1a18b34987bb2bc
Thanh Tam <thanhtamcntt12@gmail.com>
2016-12-19 20:17:37 +0700

    show-admin-word

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users, except: [:new, :create, :show]
     resources :categories
+    resources :words, only: [:index, :new, :create]
   end
 end
